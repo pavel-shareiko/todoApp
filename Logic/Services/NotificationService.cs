6ab52bc9532f5b7e6a7fecc9a01dbd632e7f8bad
pshareiko <pavel.shareiko@gmail.com>
2022-05-17 17:32:37 +0300

    Bug fix

---
+++ Logic/Services/NotificationService.cs
@@ -0,0 +1,27 @@
+﻿namespace Logic.Services
+{
+    // The class that should iterate over every task periodically.
+    // and notify the user if the deadline of the task is approaching.
+    // Scan period and notification period are configurable.
+    public class NotificationService
+    {
+        private static NotificationService _instance;
+        public static NotificationService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new NotificationService();
+                }
+                return _instance;
+            }
+        }
+
+        // Start method invokes a timer that will periodically call the method CheckDeadlines.
+        public void Start()
+        {
+            
+        }
+    }
+}
